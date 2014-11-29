952d91c6e1003792ae590574f53ca1cf5b2e80fb
Dan Kempster <dev@dankempster.co.uk>
2014-11-29 14:34:04 +0000

    Add Doctrine ORM annotation mapping to Page entity.

--- Entity/Page.php
+++ Entity/Page.php
@@ -4,6 +4,7 @@ namespace Axstrad\Bundle\PageBundle\Entity;
 use Axstrad\Component\Content\Orm\Article;
 use Axstrad\DoctrineExtensions\Activatable\ActivatableTrait;
 use Axstrad\DoctrineExtensions\Sluggable\SluggableTrait;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Cmf\Bundle\SeoBundle\SeoAwareInterface;
 use Symfony\Cmf\Bundle\SeoBundle\SeoAwareTrait;
 
