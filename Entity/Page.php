395990e070d77c15497bc57ac20f71f8de7f23a1
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2014-12-29 00:00:49 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com and modified by Dan.

--- Entity/Page.php
+++ Entity/Page.php
@@ -3,10 +3,6 @@ namespace Axstrad\Bundle\PageBundle\Entity;
 
 use Axstrad\Component\Page\Entity\BasePage;
 use Axstrad\DoctrineExtensions\Activatable\ActivatableTrait;
-use Axstrad\DoctrineExtensions\Sluggable\SluggableTrait;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Cmf\Bundle\SeoBundle\SeoAwareInterface;
-use Symfony\Cmf\Bundle\SeoBundle\SeoAwareTrait;
 
 
 /**
